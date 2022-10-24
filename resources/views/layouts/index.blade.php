5ef138a52d238d797ada0acc0d3a56b69413c1cb
BEE <darksbike44@gmail.com>
2022-10-24 22:51:50 +0800

    no message

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -75,7 +75,7 @@
                     <!-- Authentication Links -->
                     @guest
                         <li class="nav-item">
-                            <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                            <a class="nav-link" href="{{ route('login') }}">{{ __('123Login') }}</a>
                         </li>
                         @if (Route::has('register'))
                             <li class="nav-item">
